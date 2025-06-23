f0dbfd9d0cdf6ff2f0d4b44d829c030421adbabb
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-06-23 01:00:09 -0400

    added webhook obfuscator

--- backend/app/config.go
+++ backend/app/config.go
@@ -9,12 +9,13 @@ import (
 )
 
 type Config struct {
-	FN_API_KEY      string
-	FN_API_ENDPOINT string
-	DB_STRING       string
-	TELEGRAM_TOKEN  string
-	PORT            string
-	PUBLIC_URL      string
+	FN_API_KEY         string
+	FN_API_ENDPOINT    string
+	DB_STRING          string
+	TELEGRAM_TOKEN     string
+	PORT               string
+	PUBLIC_URL         string
+	WEBHOOK_OBFUSCATOR string
 }
 
 func GetConfig() *Config {
@@ -22,17 +23,18 @@ func GetConfig() *Config {
 		log.Fatalf("Could not load env vars: %s", err)
 	}
 	return &Config{
-		FN_API_KEY:      os.Getenv("FN_API_KEY"),
-		FN_API_ENDPOINT: os.Getenv("FN_API_ENDPOINT"),
-		DB_STRING:       os.Getenv("DB_STRING"),
-		TELEGRAM_TOKEN:  os.Getenv("TELEGRAM_TOKEN"),
-		PORT:            os.Getenv("PORT"),
-		PUBLIC_URL:      os.Getenv("PUBLIC_URL"),
+		FN_API_KEY:         os.Getenv("FN_API_KEY"),
+		FN_API_ENDPOINT:    os.Getenv("FN_API_ENDPOINT"),
+		DB_STRING:          os.Getenv("DB_STRING"),
+		TELEGRAM_TOKEN:     os.Getenv("TELEGRAM_TOKEN"),
+		PORT:               os.Getenv("PORT"),
+		PUBLIC_URL:         os.Getenv("PUBLIC_URL"),
+		WEBHOOK_OBFUSCATOR: os.Getenv("WEBHOOK_OBFUSCATOR"),
 	}
 }
 
 func SetupWebhook(bot *tgbotapi.BotAPI, cfg *Config) {
-	wh, err := tgbotapi.NewWebhook(cfg.PUBLIC_URL + "/webhook")
+	wh, err := tgbotapi.NewWebhook(cfg.PUBLIC_URL + "/webhook" + cfg.WEBHOOK_OBFUSCATOR) // in dev webbook obfuscator is nothing lol
 	if err != nil {
 		log.Fatal("Could not create webhook")
 	}
